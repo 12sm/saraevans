0d0235bf07dbb3d8839ed16cddd0ee2a712b278a
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-02 13:41:22 -0600

    more changes

--- template-home.php
+++ template-home.php
@@ -6,4 +6,3 @@ Template Name: Homepage Template
 
 <!--<?php get_template_part('templates/page', 'header'); ?>-->
 <?php get_template_part('templates/content', 'page-home'); ?>
-<!--Home page template is here-->
