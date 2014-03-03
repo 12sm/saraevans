3ebb4b29bbca9652bedf161ca277062531a69758
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-03 13:41:15 -0600

    added music page template

---
+++ templates/content-page-music.php
@@ -0,0 +1,6 @@
+<div class="row">
+	<div class="col-sm-12">
+		<?php echo do_shortcode('[wpv-view name="album-carousel"]') ?>
+		<?php echo do_shortcode('[wpv-view name="albums"]') ?>
+		</div>
+		</div>
