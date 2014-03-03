877e98a108ad78dfa2b69bc54ff6447c6ba86f6b
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-03 16:26:09 -0600

    fixing templates/views

--- templates/content-page-music.php
+++ templates/content-page-music.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-sm-12">
 		<?php echo do_shortcode('[wpv-view name="album-carousel"]') ?>
-		<?php echo do_shortcode('[wpv-view name="albums" ids="36"]') ?>
+		<?php echo do_shortcode('[wpv-view name="albums"]') ?>
 		</div>
 		</div>
