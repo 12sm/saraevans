b68f094bf04773a93ac2e076c75ef5d3ce74cc67
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-03 11:00:24 -0600

    insert flexslider

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,3 +68,13 @@ $(document).ready(function(){
     // Target your .container, .wrapper, .post, etc.
     $(".fitvids").fitVids();
   });
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    minItems: 3,
+  });
+});
