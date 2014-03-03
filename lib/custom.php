709f64e31c6f2fe60ca88822ea5e6c591995a946
michael <michael@12southmusic.com>
2014-03-03 17:52:00 -0600

    Add type kit to WP admin?

--- lib/custom.php
+++ lib/custom.php
@@ -2,5 +2,11 @@
 
 define( 'UPLOADS', ''.'assets' );
 
+add_action( 'admin_head' , 'add_museo_sans' );
+function add_museo_sans() { ?>
+  <script type="text/javascript" src="//use.typekit.net/cxj8bfd.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+
 
 ?>
