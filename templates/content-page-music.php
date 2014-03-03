9a2b4cadf2259248ac7f6961f64ab7b3c6d9bf55
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-03 16:16:41 -0600

    set id for music page

--- templates/content-page-music.php
+++ templates/content-page-music.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-sm-12">
 		<?php echo do_shortcode('[wpv-view name="album-carousel"]') ?>
-		<?php echo do_shortcode('[wpv-view name="albums"]') ?>
+		<?php echo do_shortcode('[wpv-view name="albums" id="36"]') ?>
 		</div>
 		</div>
