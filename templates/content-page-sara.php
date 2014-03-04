8be916aa5fe4e9aea081084ea8c7283a9a2215ad
michael <michael@12southmusic.com>
2014-03-04 13:50:12 -0600

    Add "Sara" page template

---
+++ templates/content-page-sara.php
@@ -0,0 +1,10 @@
+<div class="timeline">
+	<?php echo do_shortcode( '[WPVT]' ) ?>
+</div>
+
+<div class="col">
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php endwhile; ?>
+</div>
