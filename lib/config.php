8864e3d7c66d59e70a0e45fabc847364d7bd17af
michael <michael@12southmusic.com>
2014-03-04 12:34:27 -0600

    Put "main" classes back.

--- lib/config.php
+++ lib/config.php
@@ -20,10 +20,10 @@ define('POST_EXCERPT_LENGTH', 40); // length in words for excerpt_length filter 
 function roots_main_class() {
   if (roots_display_sidebar()) {
     // Classes on pages with the sidebar
-    $class = '';
+    $class = 'col-sm-12';
   } else {
     // Classes on full width pages
-    $class = '';
+    $class = 'col-sm-12';
   }
 
   return $class;
