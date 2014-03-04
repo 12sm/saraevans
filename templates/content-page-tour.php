7ba7baa96092fabaff416925174cbd5864bd53b8
michael <michael@12southmusic.com>
2014-03-04 14:56:56 -0600

    Put a column around the tour page

--- templates/content-page-tour.php
+++ templates/content-page-tour.php
@@ -1,3 +1,7 @@
+
+
+<div class="col">
+
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
@@ -5,3 +9,5 @@
 
 <script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
 <a href='http://www.bandsintown.com' class='bit-widget-initializer' data-text-color="#FFFFFF" data-artist='Sara Evans'>Bandsintown</a>
+
+</div>
