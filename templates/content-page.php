23280bce02b02f6e7ed1d01aeb1816c794632783
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-05 11:42:34 -0600

    blog templates

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,10 @@
+<div class="row">
+  <div class="col-sm-2">
+  </div>
+  <div class="col-sm-10">
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
+</div>
+</div>
--- templates/content.php
+++ templates/content.php
@@ -1,9 +1,17 @@
 <article <?php post_class(); ?>>
   <header>
+  <div class="row">
+    <div class="col-sm-12">
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-12">
   </header>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
+  </div>
+  </div>
   </div>
 </article>
