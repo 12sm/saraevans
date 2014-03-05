3c779581f694a7fa2fef3757310cffa18b3a6d22
Sarah <Sarah@12southmusic.com>
2014-03-05 14:15:03 -0600

    relaying out

--- templates/content-page-sara.php
+++ templates/content-page-sara.php
@@ -11,11 +11,13 @@
 
 
 <div class="col books-section">
-    <div class="row">
-       <div class="col-md-3 col-sm-3 books-heading">
-          <h1>Books</h1>
+	<div class="row">
+    	<div class="col-md-12 col-sm-12 books-heading">
+        	<h1>Books</h1>
             <h2>Sara as an Author</h2>
-       </div>
+        </div>
+    </div>
+    <div class="row">
        <div class="col-md-3 col-sm-3">
           <img src="http://saraevans.12southdev.com/assets/9781608145690_p0_v2_s260x420.jpg" align="center" />
           <h3>The Sweet By and By</h3>
@@ -28,5 +30,6 @@
           <img src="http://saraevans.12southdev.com/assets/SEnewbookcover.jpg" align="center" />
           <h3>Softly and Tenderly</h3>
        </div>
+       <div class="col-md-3 col-sm-3"></div>
     </div>
 </div><!-- end col -->
