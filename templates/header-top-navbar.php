e6a6bcf2f729cf81e401ab1abae1fb5325b63dac
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-05 11:00:24 -0600

    music button positioning

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -7,9 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <button type="button" class="btn btn-default music-btn">
-      <i class="fa fa-music fa-2x"></i>
-      </button>
+     
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
 
@@ -20,5 +18,8 @@
         endif;
       ?>
     </nav>
+     <button type="button" class="btn btn-default music-btn">
+      <i class="fa fa-music fa-2x"></i>
+      </button>
   <!-- </div> -->
 </header>
