6ea42b0e3924bc30e0f8c64f298ac206e51dbce9
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-05 15:00:09 -0600

    ch-ch-ch-changes

--- templates/content.php
+++ templates/content.php
@@ -1,1 +1,24 @@
-<?php echo do_shortcode('[wpv-view name="blog-page"]') ?>
+
+<div class="row">
+  <div class="col-sm-2">
+  </div>
+  <div class="col-sm-10"><article <?php post_class(); ?>>
+  <header>
+  <div class="row">
+    <div class="col-sm-12">
+    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php get_template_part('templates/entry-meta'); ?>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-12">
+  </header>
+  <div class="entry-summary">
+    <?php the_excerpt(); ?>
+  </div>
+  </div>
+  </div>
+</article>
+</div>
+</div>
+</div>
