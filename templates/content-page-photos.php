f2a6893a8c49a631e5e6f42b3d28be7a9ed472e9
michael <michael@12southmusic.com>
2014-03-04 18:08:04 -0600

    Photos page template

---
+++ templates/content-page-photos.php
@@ -0,0 +1,9 @@
+<div class="col">
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php endwhile; ?>
+</div>
+
+<?php echo do_shortcode( '[yinstagram]' ) ?>
