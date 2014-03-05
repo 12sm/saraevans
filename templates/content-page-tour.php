db6dab4c699c4a776449ecbc89e92b61a81f73a4
Sarah <Sarah@12southmusic.com>
2014-03-05 17:43:01 -0600

    changing default text color in bands in town embed

--- templates/content-page-tour.php
+++ templates/content-page-tour.php
@@ -9,6 +9,6 @@
 
 <h1>Sarah on Tour</h1>
 <script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
-<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-text-color="#FFFFFF" data-artist='Sara Evans'>Bandsintown</a>
+<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-text-color="#898989" data-artist='Sara Evans'>Bandsintown</a>
 
 </div>
