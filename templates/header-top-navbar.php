98ccf328187b9f447c6ec8ca0ad3f46c5ec85164
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-05 11:23:42 -0600

    link to music page

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -17,9 +17,9 @@
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills nav-stacked'));
         endif;
       ?>
-      <button type="button" class="btn btn-default music-btn">
+      <a href="/music"><button type="button" class="btn btn-default music-btn">
       <i class="fa fa-music fa-2x"></i>
-      </button>
+      </button></a>
     </nav>
 
   <!-- </div> -->
