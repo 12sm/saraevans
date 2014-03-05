a7ca8a5c25d08ff3a1f5283fb9066d8e3f5bbba3
Sarah <Sarah@12southmusic.com>
2014-03-05 14:16:05 -0600

    adding links

--- templates/content-page-sara.php
+++ templates/content-page-sara.php
@@ -19,15 +19,21 @@
     </div>
     <div class="row">
        <div class="col-md-3 col-sm-3">
+          <a href="#">
           <img src="http://saraevans.12southdev.com/assets/9781608145690_p0_v2_s260x420.jpg" align="center" />
+          </a>
           <h3>The Sweet By and By</h3>
        </div>
        <div class="col-md-3 col-sm-3">
+          <a href="#">
           <img src="http://saraevans.12southdev.com/assets/lovelifted2g.jpg" align="center" />
+          </a>
           <h3>Love Lifted Me</h3>
        </div>
        <div class="col-md-3 col-sm-3">
+          <a href="#">
           <img src="http://saraevans.12southdev.com/assets/SEnewbookcover.jpg" align="center" />
+          </a>
           <h3>Softly and Tenderly</h3>
        </div>
        <div class="col-md-3 col-sm-3"></div>
