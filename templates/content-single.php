f6bbd08d4b8f90529d334bb4c1ef92cfcea46650
Sarah <Sarah@12southmusic.com>
2014-03-05 16:38:02 -0600

    adding a class

--- templates/content-single.php
+++ templates/content-single.php
@@ -4,7 +4,7 @@
 
 <div class="col">
   <article <?php post_class(); ?>>
-    <?php echo get_the_post_thumbnail($post_id, 'full'); ?> 
+    <?php echo get_the_post_thumbnail($post_id, 'full', array('class' => 'post-image')); ?> 
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
