055d2345d87cc40e839d7f6e43d8e1a3658d7144
Sarah <Sarah@12southmusic.com>
2014-03-05 14:11:26 -0600

    nope, thats ugly

--- templates/content-page-sara.php
+++ templates/content-page-sara.php
@@ -18,17 +18,17 @@
         </div>
     </div>
     <div class="row">
-       <div class="col-md-4 col-sm-4 single-book">
+       <div class="col-md-4 col-sm-4">
           <img src="http://saraevans.12southdev.com/assets/9781608145690_p0_v2_s260x420.jpg" align="center" />
           <h3>The Sweet By and By</h3>
           <p>Jade Fitzgerald left the pain of her past in the dust when she headed out for college a decade ago. Now she's thriving in her career and glowing in the light of Max Benson's love. But then Jade's hippie mother, Beryl Hill, arrives in Whisper Hollow, Tennessee, for Jade's wedding along with Willow, her wild younger sister. Their arrival forces Jade to throw open the dark closets of her past--the insecurity of living with a restless, wandering mother, the silence of her absent father, and the heart-ripping pain of first-love's rejection.</p>
        </div>
-       <div class="col-md-4 col-sm-4 single-book">
+       <div class="col-md-4 col-sm-4">
           <img src="http://saraevans.12southdev.com/assets/lovelifted2g.jpg" align="center" />
           <h3>Love Lifted Me</h3>
           <p>Jade and Max share a deep love, though revelations from his past have recently shaken their marriage. And Jade is completely smitten with Max's little son, Asa, whom she is now raising as her own. Their blended family brings her a joy she's never known. But there is one more secret to be uncovered. One that will impact them all.</p>
        </div>
-       <div class="col-md-4 col-sm-4 single-book">
+       <div class="col-md-4 col-sm-4">
           <img src="http://saraevans.12southdev.com/assets/SEnewbookcover.jpg" align="center" />
           <h3>Softly and Tenderly</h3>
           <p>Happily married and owner of two successful boutiques, Jade longs to begin a family with her husband, Max. But when she discovers that Max has an illegitimate son—who he wants her to help raise—Jade’s life is turned upside down.</p>
