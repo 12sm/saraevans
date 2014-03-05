23a6ab2a5d80e9115dad433dca04d5cc33600399
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-05 15:55:43 -0600

    more commits

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,15 @@
+<<<<<<< HEAD
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
+=======
+<?php echo do_shortcode('[wpv-view name="blog-page"]') ?>
+
+<div class="col">
+<!--<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php endwhile; ?>-->
+</div>
+>>>>>>> e9f28d5df896a3babbc89c90aeed335d77c61f54
