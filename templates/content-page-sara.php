dfe7c43b950f959a3eab4ee52d5a32f192de2c85
Sarah <Sarah@12southmusic.com>
2014-03-05 14:32:49 -0600

    nope don't like it, changing it back
    this is taking me entirely too long

--- templates/content-page-sara.php
+++ templates/content-page-sara.php
@@ -12,14 +12,12 @@
 
 <div class="col books-section">
 	<div class="row">
-    	<div class="col-md-1 col-sm-1"></div>
-    	<div class="col-md-9 col-sm-11 books-heading">
+    	<div class="col-md-12 col-sm-12 books-heading">
         	<h1>Books</h1>
             <h2>Sara as an Author</h2>
         </div>
     </div>
     <div class="row">
-       <div class="col-md-1 col-sm-1"></div>
        <div class="col-md-3 col-sm-3">
           <a href="http://saraevans.merchmo.com/pages/productdetail/Sweet_By_And_By_Book_264/">
           <img src="http://saraevans.12southdev.com/assets/9781608145690_p0_v2_s260x420.jpg" align="center" />
@@ -38,5 +36,7 @@
           </a>
           <h3>Softly and Tenderly</h3>
        </div>
+       <div class="col-md-3 col-sm-3"></div>
+
     </div>
 </div><!-- end col -->
