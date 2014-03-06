1387b19a2e85a6d81bcb5304696ae680d4950a0a
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-06 13:05:47 -0600

    socials launch to new page

--- templates/footer.php
+++ templates/footer.php
@@ -19,11 +19,11 @@
               </div>
               <div class="col-sm-6">
       <div id="social" class="row social-nav">
-  <ul id="menu-social-navigation" class=""><li class="menu-"><a href="https://www.facebook.com/saraevans"><i class="fa fa-facebook fa-2x"></i></a></li>
-<li class="menu-"><a href="https://twitter.com/saraevansmusic"><i class="fa fa-twitter fa-2x"></i></a></li>
-<li class="menu-"><a href="http://instagram.com/saraevansmusic"><i class="fa fa-instagram fa-2x"></i></a></li>
-<li class="menu-"><a href="http://www.pinterest.com/saraevansmusic/‎"><i class="fa fa-pinterest fa-2x"></i></a></li>
-<li class="menu-"><a href="http://www.youtube.com/user/saraevans"><i class="fa fa-youtube-square fa-2x"></i></a></li>
+  <ul id="menu-social-navigation" class=""><li class="menu-"><a href="https://www.facebook.com/saraevans_blank"><i class="fa fa-facebook fa-2x"></i></a></li>
+<li class="menu-"><a href="https://twitter.com/saraevansmusic_blank"><i class="fa fa-twitter fa-2x"></i></a></li>
+<li class="menu-"><a href="http://instagram.com/saraevansmusic_blank"><i class="fa fa-instagram fa-2x"></i></a></li>
+<li class="menu-"><a href="http://www.pinterest.com/saraevansmusic/‎_blank"><i class="fa fa-pinterest fa-2x"></i></a></li>
+<li class="menu-"><a href="http://www.youtube.com/user/saraevans_blank"><i class="fa fa-youtube-square fa-2x"></i></a></li>
 </ul>
               </div>
             </div>
