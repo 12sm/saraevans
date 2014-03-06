9f74512854e177147987b423e87466415bdfa2c4
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-05 18:17:37 -0600

    diff temp

--- taxonomy-galleries-other.php
+++ taxonomy-galleries-other.php
@@ -1,1 +1,19 @@
-<?php echo do_shortcode('[wpv-view name="video-page"]') ?>
+<div class="row">
+  <div class="col-md-2">
+  </div>
+  <div class="col-md-8"> 
+  <div class="row">
+  <h1><?php echo do_shortcode('[wpv-post-title]')?></h1>
+  </div>
+  <div class="row fitvids">
+      <?php echo do_shortcode('[types field="video-url" width="1280" height="720"][/types]')?>  
+  </div>
+      <div class="row">
+        <div class="col-xs-6">
+        </div>
+        <div class="col-xs-6">
+        </div>
+      </div>
+  </div>
+</div>
+<?php echo do_shortcode('[wpv-view name="taxonomy-gallery-view"]')
