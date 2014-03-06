54bc80966e9cf9fa37f2a82156a637c9316e4ec3
michael <michael@12southmusic.com>
2014-03-06 16:52:12 -0600

    New single video template

--- single-video.php
+++ single-video.php
@@ -1,7 +1,13 @@
+
+<?php while (have_posts()) : the_post(); ?>
+
 <div class="row col-video">
   <div class="col-md-12"> 
   <div class="row">
-  <h1><?php echo do_shortcode('[wpv-post-title]')?></h1>
+  	<header>
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <?php get_template_part('templates/entry-meta'); ?>
+      </header>
   </div>
   <div class="row fitvids">
       <?php echo do_shortcode('[types field="video-url" width="1280" height="720"][/types]')?>  
@@ -14,4 +20,14 @@
       </div>
   </div>
 </div>
+
 <?php echo do_shortcode('[wpv-view name="Related Videos"]')?>
+
+    <div class="row col">
+    	<div class="col-md-12 col-sm-12">
+    		<?php comments_template('/templates/comments.php'); ?>
+        </div>
+    </div>
+
+
+<?php endwhile; ?>
