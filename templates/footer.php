cdf2bb6230d55cdf84bd1ef4c738d4768b1ba394
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-06 15:53:11 -0600

    closing divs

--- templates/footer.php
+++ templates/footer.php
@@ -35,9 +35,6 @@
               </div>
             </div>
             </div>
-          </div>
-        </div>
-        </div>
       <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
     </div>
   </div>
