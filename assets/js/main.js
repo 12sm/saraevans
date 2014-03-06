71e01f82635fc664829d1fff2abe5b142b9e78c4
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-06 15:06:59 -0600

    more jquery fiddling

--- assets/js/main.js
+++ assets/js/main.js
@@ -217,8 +217,10 @@ var UTIL = {
 
 $(document).ready(UTIL.loadEvents);
 })(jQuery); // Fully reference jQuery after this point.
+$("#e2ma_signup").load(function(){
+  $("#e2ma_signup_submit_button" ).val( "JOIN" );
+})
 
-$( "#e2ma_signup_submit_button" ).val( "JOIN" );
 
 
 
