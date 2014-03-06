140fcc37f300ed9bcfb1051696e8fc1cfed43cbc
michael <michael@12southmusic.com>
2014-03-06 14:06:29 -0600

    Update video templates

--- single-video.php
+++ single-video.php
@@ -1,7 +1,5 @@
-<div class="row">
-  <div class="col-md-2">
-  </div>
-  <div class="col-md-8"> 
+<div class="row col">
+  <div class="col-md-12"> 
   <div class="row">
   <h1><?php echo do_shortcode('[wpv-post-title]')?></h1>
   </div>
--- templates/content-page-video.php
+++ templates/content-page-video.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row col">
 <div class="col-sm-12">
 <?php echo do_shortcode('[wpv-view name="Video Page"]') ?>
 </div>
