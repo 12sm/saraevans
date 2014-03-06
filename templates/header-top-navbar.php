ea914d2f16919e2b765a24e9d182343878dd19b7
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-06 12:19:42 -0600

    social floater

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -10,6 +10,15 @@
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
 
+   <div id="social-float" class="row social-nav">
+  <ul id="menu-social-navigation" class=""><li class="menu-"><a href="https://www.facebook.com/saraevans"><i class="fa fa-facebook fa-2x"></i></a></li>
+<li class="menu-"><a href="https://twitter.com/saraevansmusic"><i class="fa fa-twitter fa-2x"></i></a></li>
+<li class="menu-"><a href="http://instagram.com/saraevansmusic"><i class="fa fa-instagram fa-2x"></i></a></li>
+<li class="menu-"><a href="http://www.pinterest.com/saraevansmusic/‎"><i class="fa fa-pinterest fa-2x"></i></a></li>
+<li class="menu-"><a href="http://www.youtube.com/user/saraevans"><i class="fa fa-youtube-square fa-2x"></i></a></li>
+</ul>
+              </div>
+
     <nav class="collapse navbar-collapse affix" role="navigation"> 
       <?php
         if (has_nav_menu('primary_navigation')) :
