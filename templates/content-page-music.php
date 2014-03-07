91f7ee8f4fdcb2762e37f80b2d5f36eae9f07a26
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-07 17:59:46 -0600

    carousel container and min height

--- templates/content-page-music.php
+++ templates/content-page-music.php
@@ -1,6 +1,10 @@
 <div class="row">
 	<div class="col-sm-12">
+	<div class="row">
+		<div class="col-sm-12 vid-carousel-container">
 		<?php echo do_shortcode('[wpv-view name="album-carousel"]') ?>
+		</div>
+		</div>
 		<?php echo do_shortcode('[wpv-view name="albums"]') ?>
 		</div>
 		</div>
