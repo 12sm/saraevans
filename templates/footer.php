2ff9fd88c2a1115053e60af9917d8d8cdbfcf522
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-07 16:49:47 -0600

    credits styling

--- templates/footer.php
+++ templates/footer.php
@@ -29,7 +29,7 @@
 					  wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
 					endif;
 				  ?>
-                  <div class="row col">
+                  <div class="row">
       <div class="col-sm-12">  
       <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
       </div>
