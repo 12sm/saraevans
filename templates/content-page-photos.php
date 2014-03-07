c2eb14eaa28ab8da93a6dc9f5ce3547f0491d2b6
michael <michael@12southmusic.com>
2014-03-06 19:02:28 -0600

    Photos page

--- templates/content-page-photos.php
+++ templates/content-page-photos.php
@@ -1,9 +1,9 @@
 <div class="col">
-
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
-</div>
 
 <ul class="insta-grid"></ul>
+
+</div>
