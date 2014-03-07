79ac301257ead8a69b7bd1bb547ec29e2e4c101a
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-07 16:40:59 -0600

    col-lg

--- templates/footer.php
+++ templates/footer.php
@@ -13,10 +13,10 @@
             <div class="row col">
               <div class="col-sm-7 mailing-list">
                 <div class="row">
-                  <div class="col-md-6">
+                  <div class="col-lg-6">
               		<h2 class="join-list">JOIN THE MAILING LIST</h2>
                   </div>
-                  <div class="col-md-6">
+                  <div class="col-lg-6">
                   <script type="text/javascript" src="https://app.e2ma.net/app2/audience/tts_signup/1758490/c5eee1896752e5ac19a3a0bb34fbab4b/7712/?v=a"></script>
                     <div id="load_check" class="signup_form_message" >This form needs Javascript to display, which your browser doesn't support. <a href="https://app.e2ma.net/app2/audience/signup/1758490/7712/?v=a"> Sign up here</a> instead </div>
 					<script type="text/javascript">signupFormObj.drawForm();</script>
