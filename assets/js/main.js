c02b3a21d0bf94a5f198c46e6afcff07e935ed2d
michael <michael@12southmusic.com>
2014-03-10 01:55:51 -0500

    Last minute changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -154,18 +154,8 @@ var Roots = {
         // Keyboard shortcuts
         $(document).keydown(function(e) {
           var unicode = e.charCode ? e.charCode : e.keyCode;
-             // right arrow
-          if (unicode == 39) {
-            var next = $('li.playing').next();
-            if (!next.length) next = $('ol li').first();
-            next.click();
-            // back arrow
-          } else if (unicode == 37) {
-            var prev = $('li.playing').prev();
-            if (!prev.length) prev = $('ol li').last();
-            prev.click();
-            // spacebar
-          } else if (unicode == 32) {
+             // space bar
+          if (unicode == 32) {
             audio.playPause();
           }
         })
