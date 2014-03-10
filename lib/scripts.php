25be1ec24e597d88ca5c57f5cc1ca56d8d64a3b2
michael <michael@12southmusic.com>
2014-03-10 03:02:09 -0500

    Push new analytics code

--- lib/scripts.php
+++ lib/scripts.php
@@ -55,13 +55,17 @@ function roots_jquery_local_fallback($src, $handle = null) {
 add_action('wp_head', 'roots_jquery_local_fallback');
 
 function roots_google_analytics() { ?>
-<script>
-  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-  function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-  e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-  e.src='//www.google-analytics.com/analytics.js';
-  r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-  ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>');ga('send','pageview');
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+  	_gaq.push(['_setAccount', '<?php echo GOOGLE_ANALYTICS_ID; ?>']);
+  	_gaq.push(['_setDomainName', 'saraevans.com']);
+  	_gaq.push(['_setAllowLinker', true]);
+  	_gaq.push(['_trackPageview']);
+ 	(function() {
+	  	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	  	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	  	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  	})();
 </script>
 
 <?php }
