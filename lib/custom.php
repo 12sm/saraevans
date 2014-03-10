c35bfae55b883771a873b35c10d8f9c45ad7eaf2
michael <michael@12southmusic.com>
2014-03-10 11:55:46 -0500

    Remove extra HTML outputted by Views / Access

--- lib/custom.php
+++ lib/custom.php
@@ -19,4 +19,20 @@ return yoast_breadcrumb('<div class="breadcrumbs">','</div>');
 add_shortcode('breadcrumbs', 'bread_crumbs');
 
 
+add_action( 'init', 'my_cleanup', 11 ); // after they are registered
+function my_cleanup() {
+if ( !is_admin() ) {
+global $WP_Views;
+remove_action('wp_print_styles', array($WP_Views, 'add_render_css'));
+remove_action('wp_head', 'wpv_add_front_end_js');
+wp_dequeue_style( 'views-pagination-style' );
+wp_dequeue_script( 'views-pagination-script' );
+wp_dequeue_script( 'wpv-date-front-end-script' );
+wp_dequeue_script( 'jquery-ui-datepicker' );
+wp_dequeue_script( 'jquery-ui-core' );
+wp_dequeue_style( 'jquery-ui-datepicker' );
+}
+}
+
+
 ?>
