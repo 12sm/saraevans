798d26c978aa8ab2da5b34111612b6845c493c9d
michael <michael@12southmusic.com>
2014-03-22 03:35:39 -0500

    Revert "Push brand"
    This reverts commit fb3cfe79f825f19c31b809e4c1cb9e5c6e0c7be5.

--- templates/head.php
+++ templates/head.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<![CDATA[
+<!DOCTYPE html><!-- 
    _      ___    ____                
  /' \   /'___`\ /\  _`\   /'\_/`\    
 /\_, \ /\_\ /\ \\ \,\L\_\/\      \   
@@ -11,7 +10,7 @@
 built by 12South Music
 http://12southmusic.com/
  
-]]>
+ -->
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
