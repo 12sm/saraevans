364ea6fffeed83a9712e4bf8fe57b80c5d7e3de7
Sarah <Sarah@12southmusic.com>
2014-05-15 14:12:18 -0500

    meet n greet linkage!

--- templates/content-page-tour.php
+++ templates/content-page-tour.php
@@ -8,6 +8,11 @@
 <?php endwhile; ?>
 
 <h1>Sara on Tour</h1>
+
+<a href="http://saraevans.com/meet-greet-photos/">
+<img src="http://saraevans.com/assets/meet-n-greet.png" />
+</a>
+
 <script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
 <a href='http://www.bandsintown.com' class='bit-widget-initializer' data-text-color="#898989" data-artist='Sara Evans'>Bandsintown</a>
 
