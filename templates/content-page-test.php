41f510ec310ef0a258d1730389fd813706c684e7
Ian Noyes <ian@12southmusic.com>
2014-10-23 17:49:28 -0500

    add revo slider

---
+++ templates/content-page-test.php
@@ -0,0 +1,63 @@
+<div class="row">
+<div class="col-md-12 tk-omnes-pro top-slider">
+<?php echo do_shortcode( '[rev_slider home]') ?>
+</div>
+</div>
+
+<div class="col">
+
+<div class="row">
+  <div class="col-md-6">
+    <div class="row">
+      <div class="col-md-12">
+      <?php echo do_shortcode( '[wpv-view name="home-banner"]' ) ?>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6 bands-in-town-section">
+      <div class="row">
+      <div class="col-md-12">
+        <h1 class="bands-in-town-h1">Sara on Tour</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 bands-in-town-embed">
+      <script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
+<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-display-limit="4" data-text-color="none" data-artist='Sara Evans'>Bandsintown</a>
+      </div>
+    </div>
+        <div class="row">
+      <div class="col-md-12">
+        <button type="button" onclick="window.location.href='/tour'" href="/tour">More Dates</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-12 add-margin">&nbsp;</div>
+</div>
+
+<div class="row">
+  <div class="col-md-3 fitvids instagram-section">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="instagram-h1">Instagram</h1>
+      </div>
+    </div>
+    <div class="row">
+      <ul class="col-md-12 insta-grid">
+      </ul>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <button type="button" onclick="window.open('http://instagram.com/saraevansmusic')" href="http://instagram.com/saraevansmusic"><i class="fa fa-instagram"></i> @saraevansmusic</button>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-9 fitvids">
+    <?php echo do_shortcode( '[wpv-view name="video home"]' ) ?>
+  </div>
+</div>
+
+</div>
