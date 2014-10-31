bb641a8ffad5b3a4a3121332547badfe358287d8
Ian Noyes <ian@12southmusic.com>
2014-10-31 13:29:56 -0500

    cbox options

--- assets/js/main.js
+++ assets/js/main.js
@@ -221,7 +221,10 @@ var Roots = {
   },
   single_photo_galleries: {
     init: function(){
-      $('.img-thumbnail').colorbox();
+      $('.img-thumbnail').colorbox({
+        maxWidth: 80%,
+        maxHeight: 768px
+      });
       $('.img-thumbnail').attr('rel', 'gal');
     }
   }
