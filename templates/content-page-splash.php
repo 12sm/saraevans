78e3f77e40e28c4df422e67da718ab959e0bd9fa
Ian Noyes <ian@12southmusic.com>
2014-12-09 11:44:38 -0600

    splash templates

---
+++ templates/content-page-splash.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="EN">
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+  
+  <title>Sara Evans | Sara Evans | 12 Days At Christmas</title>
+    <meta name="viewport" content="user-scalable=yes">
+
+    
+    <meta name="title" content="Sara Evans | Sara Evans | 12 Days At Christmas" />
+  <meta name="description" content="Tweet and enter daily for a chance to win great prizes from @SaraEvansMusic's Christmas List! http://go2w.in/saraevans" />
+  <link rel="image_src" href="http://www.tweematic.com/saraevans/images/icon.png" / >
+
+  <!-- Facebook Like -->
+      <meta property="og:title" content="Sara Evans | 12 Days At Christmas"/>
+    <meta property="og:site_name" content="Tweematic"/>
+    <meta property="og:image" content="http://www.tweematic.com/saraevans/images/icon.png"/>
+    <meta property="og:url" content="http://www.tweematic.com/saraevans/"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:description" content="Tweet and enter daily for a chance to win great prizes from @SaraEvansMusic's Christmas List! http://go2w.in/saraevans"/>
+
+
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:title" content="Sara Evans | Sara Evans | 12 Days At Christmas" />
+  <meta name="twitter:description" content="Tweet and enter daily for a chance to win great prizes from @SaraEvansMusic's Christmas List! http://go2w.in/saraevans" />
+  <meta name="twitter:image" content="http://www.tweematic.com/caseyjames/images/album.png" />
+  <!-- Schema.org markup for Google+ -->
+  <meta itemprop="name" content="Sara Evans | Sara Evans | 12 Days At Christmas">
+  <meta itemprop="description" content="Tweet and enter daily for a chance to win great prizes from @SaraEvansMusic's Christmas List! http://go2w.in/saraevans">
+  <meta itemprop="image" content="http://www.tweematic.com/caseyjames/images/icon.png">
+  <style type="text/css">
+  .node-title{ display:none; width: 100%; height: 1px;}
+    html {overflow: auto;}
+    html, body, div, iframe {margin: 0px; padding: 0px; height: 100%; border: none;}
+    iframe {display: block; width: 100%; border: none; overflow-y: auto; overflow-x: hidden; height:100%;}
+  </style>
+
+  </head>
+  <body>
+    <!-- Tweematic Iframe Container v2.1 -->
+    <iframe id="main" name="main" src="http://www.tweematic.com/saraevans/" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="100%" scrolling="auto"></iframe>
+<script>
+var projectWidth=1000;
+var projectHeight=1100;
+</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="//d3f6omxqx4kosh.cloudfront.net/iframe/js/iframe.js"></script>
+
+</body>
+</html>
