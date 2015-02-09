c76efa88b7760a03e2c3e48e76590941506e9774
Sarah <Sarah@12southmusic.com>
2015-02-09 16:29:37 -0600

    testing

---
+++ template-tour-tour.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: Tour Template try again
+*/
+?>
+
+<!--<?php get_template_part('templates/page', 'header'); ?>-->
+<?php get_template_part('templates/content', 'page-tour'); ?>
+
+test
