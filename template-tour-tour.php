b23e9fdc58aa8d2992c0f09a9cc6ab6a25209908
Sarah <Sarah@12southmusic.com>
2015-02-09 16:40:36 -0600

    removing old tour stuff

--- template-tour-tour.php
+++ template-tour-tour.php
@@ -5,5 +5,4 @@ Template Name: Tour Template for Songkick Tourbox
 ?>
 
 <!--<?php get_template_part('templates/page', 'header'); ?>-->
-<?php get_template_part('templates/content', 'page-tour'); ?>
 <?php get_template_part('templates/content', 'page-tour-js'); ?>
